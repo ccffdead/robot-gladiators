24e9d2c392e3ca6aec01bd53fb323df966cd1963
ccffdead <ccffdead@gmail.com>
2021-10-18 15:20:40 -0400

    added skip window alert

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-
 var playerAttack = 10;
+var playerMoney = 10;
 
 // console logs
 console.log(playerName, playerAttack, playerHealth);
@@ -13,6 +13,15 @@ var enemyAttack = 12;
 var fight = function() {
     //alert players they are starting a new round
     window.alert("Welcome to Robot Gladiators!");
+
+    //fight or skip prompt
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    console.log(
+        playerName + " chooses to " + promptFight + " the battle!"
+    )
+    // if player choses to fight, then fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+
         // subtract the value of "playerAttack" from the value of "enemyHealth" and use that result to update the value in the "enemyHealth" variable
         enemyHealth = enemyHealth - playerAttack;
         
@@ -41,7 +50,26 @@ var fight = function() {
                  } 
                 else {
                     window.alert(playerName + " still has " + playerHealth + " health left.");
-  }
+                }
+    // if player choses to skip
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+            // if yes (true), leave fight
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                    // subtract money from playerMoney for skipping
+                     playerMoney = playerMoney - 2;
+            }
+            // if no (false), ask question again by running fight() again
+             else {
+                fight();
+            }
+    } else {
+    window.alert("You need to choose a valid option. Try again!");
+    }
 };
 
 //execute function
